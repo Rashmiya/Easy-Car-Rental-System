99d2b6d6c62939c613ab2eb6f3fd765bb842f6e8
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-28 17:34:36 +0530

    Customer Registration form ui completed

--- FrontEnd/controller/mainDashboard.js
+++ FrontEnd/controller/mainDashboard.js
@@ -19,23 +19,60 @@ $("#customerRegisterPage").css('display','block');
 
 /*when click login button in HomePage*/
 $("#button2").on('click',function (){
-    $("#customerLoginPage").css('display','block');
-
     $("#homeContainer").css('display','none');
     $("#aboutUsContainer").css('display','none');
     $("#servicesContainer").css('display','none');
     $("#fleetContainer").css('display','none');
     $("#contactContainer").css('display','none');
+
+    $("#customerLoginPage").css('display','block');
 });
-
+/*when click back icon in Customer SignIn page*/
 $("#backIcon").on('click',function (){
-    $("#customerLoginPage").css('display','none');
-
     $("#homeContainer").css('display','block');
     $("#aboutUsContainer").css('display','block');
     $("#servicesContainer").css('display','block');
     $("#fleetContainer").css('display','block');
     $("#contactContainer").css('display','block');
+
+    $("#customerLoginPage").css('display','none');
 });
+/*when click back icon in Customer Register page*/
+$("#RegistrationBackIcon").on('click',function (){
+    $("#homeContainer").css('display','block');
+    $("#aboutUsContainer").css('display','block');
+    $("#servicesContainer").css('display','block');
+    $("#fleetContainer").css('display','block');
+    $("#contactContainer").css('display','block');
+
+    $("#customerLoginPage").css('display','none');
+    $("#customerRegisterPage").css('display','none');
+});
+/*when click create a new Account in Login page*/
+$("#NewAccount").on('click',function (){
+    $("#homeContainer").css('display','none');
+    $("#aboutUsContainer").css('display','none');
+    $("#servicesContainer").css('display','none');
+    $("#fleetContainer").css('display','none');
+    $("#contactContainer").css('display','none');
+
+    $("#customerLoginPage").css('display','none');
+    $("#customerRegisterPage").css('display','block');
+});
+/*when click have an account text in customer Registration Page*/
+$("#backToLogin").on('click',function (){
+    $("#homeContainer").css('display','none');
+    $("#aboutUsContainer").css('display','none');
+    $("#servicesContainer").css('display','none');
+    $("#fleetContainer").css('display','none');
+    $("#contactContainer").css('display','none');
+
+    $("#customerLoginPage").css('display','block');
+    /*$("#customerLoginPage").css('height' ,'90vh');*/
+    $("#customerRegisterPage").css('display','none');
+   /* $("#customerRegisterPage").css('height' ,'90vh');*/
+});
+
+
 //other
 $("#customerLoginDetailsConatiner1").css('display','none');
